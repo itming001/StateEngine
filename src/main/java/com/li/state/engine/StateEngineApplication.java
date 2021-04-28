78ee5055b231b2cdbe944384f86357c31d6a7274
liyanming <itming001@163.com>
2021-04-28 14:11:53 +0800

    初始化项目

--- src/main/java/com/li/state/engine/StateEngineApplication.java
+++ src/main/java/com/li/state/engine/StateEngineApplication.java
@@ -3,6 +3,9 @@ package com.li.state.engine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lym
+ */
 @SpringBootApplication
 public class StateEngineApplication {
 
