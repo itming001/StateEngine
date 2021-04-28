302c5481f68d16216c05b319a3808614050b37b8
liyanming <itming001@163.com>
2021-04-28 14:09:54 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# StateEngine
-实现状态引擎模板方式的编写，基于公众号（https://mp.weixin.qq.com/s/0GfCOUEw4svvSQVoShjJDw ）进行代码书写
+# 工程简介
+
+
+
+# 延伸阅读
+
---
+++ src/main/java/com/li/state/engine/StateEngineApplication.java
@@ -0,0 +1,13 @@
+package com.li.state.engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StateEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StateEngineApplication.class, args);
+    }
+
+}
